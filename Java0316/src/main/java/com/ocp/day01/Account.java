36dc3f853bb7b775fb1c39c64b8090a20da6fe3d
MB-study <MB-study@192.168.1.50>
2021-03-16 21:41:26 +0800

    java

--- Java0316/src/main/java/com/ocp/day01/Account.java
+++ Java0316/src/main/java/com/ocp/day01/Account.java
@@ -1,11 +1,31 @@
 package com.ocp.day01;
 
 public class Account {
-    int balance; // 帳戶餘額
-    void withdraw() { // 提款方法
-        
+    private int balance; // 帳戶餘額
+    //查詢帳戶餘額
+    int getBalance(){
+        return balance;
+    }
+    
+    void withdraw(int money) { // 提款方法
+        // 驗證 money 是否符合存款的條件
+        if(money <= 0) {
+            System.out.println("提款金額不符!");
+            return; // 提款方法到此結束 !
+        }
+        // 驗證 money 是否大於 balance ?
+        if(money > balance) {
+            System.out.println("帳戶餘額不足!");
+            return; // 提款方法到此結束 !
+        }
+        balance -= money;
     }
     void deposit(int money) { // 存款方法
+        // 驗證 money 是否符合存款的條件
+        if(money <= 0) {
+            System.out.println("存款金額不符!");
+            return; // 存款方法到此結束 !
+        }
         balance += money;
     }
 }
