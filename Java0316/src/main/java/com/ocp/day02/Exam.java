75584bcb8f04840f25e76d0f303fbc30e3b500e1
MB-study <MB-study@192.168.1.50>
2021-03-18 20:48:02 +0800

    java

--- Java0316/src/main/java/com/ocp/day02/Exam.java
+++ Java0316/src/main/java/com/ocp/day02/Exam.java
@@ -1,52 +1,47 @@
-
 package com.ocp.day02;
 
-
 public class Exam {
+
     //屬性
     String name;
     int chinese;
     int math;
-    
+
     //建構子
-    Exam (String n, int c, int m) {
+    Exam(String n, int c, int m) {
         this.name = n;
         this.chinese = c;
         this.math = m;
     }
-    
-    Exam (){
-        
+
+    Exam() {
+
     }
-    
+
     //計算方法
-    int getSum(){
+    int getSum() {
         int sum = chinese + math;
         return sum;
     }
-    
-    double getAvg(){
-        double avg = getSum()/2.0;
+
+    double getAvg() {
+        double avg = getSum() / 2.0;
         return avg;
     }
-    
-    String getResultValue (){
+
+    String getResultValue() {
         double avg = getAvg();
-        String result = (avg > 60)? "及格":"不及格";
+        String result = (avg > 60) ? "及格" : "不及格";
         return result;
     }
     //void print(){
-        //int sum = getSum();
-        //double avg = getAvg();
-        //System.out.printf("姓名: %s, 國文: %d, 數學: %d, 總分: %d, 平均: %.1f, 結果:%s\n", name,chinese,math,sum,avg,result);
-    
-    String getPrintData(){
-        return String.format("姓名: %s, 國文: %d, 數學:%d, 總分: %d, 平均: %.1f, 結果: %s\n", name,chinese,math,getSum(),getAvg(),getResultValue());
-        
-        
-    
-    
+    //int sum = getSum();
+    //double avg = getAvg();
+    //System.out.printf("姓名: %s, 國文: %d, 數學: %d, 總分: %d, 平均: %.1f, 結果:%s\n", name,chinese,math,sum,avg,result);
+
+    String getPrintData() {
+        return String.format("姓名: %s, 國文: %d, 數學:%d, 總分: %d, 平均: %.1f, 結果: %s\n", name, chinese, math, getSum(), getAvg(), getResultValue());
+
     }
-    
-    
+
 }
--- Java0316/src/main/java/com/ocp/day02/ExamCase1.java
+++ Java0316/src/main/java/com/ocp/day02/ExamCase1.java
@@ -1,28 +1,25 @@
-
 package com.ocp.day02;
 
-
 public class ExamCase1 {
+
     public static void main(String[] args) {
-        Exam a1 = new Exam("小明", 90,70);        
+        Exam a1 = new Exam("小明", 90, 70);
         System.out.println(a1.getPrintData());
-        
+
         Exam a2 = new Exam();
-        a2.name="小英";
-        a2.chinese=32;
-        a2.math=55;
+        a2.name = "小英";
+        a2.chinese = 32;
+        a2.math = 55;
         System.out.println(a2.getPrintData());
-        
+
         Exam a3 = new Exam("小華", 87, 50);
         System.out.println(a3.getPrintData());
-        
+
         Exam a4 = new Exam();
         a4.name = "小美";
-        a4.chinese=82;
-        a4.math=45;
+        a4.chinese = 82;
+        a4.math = 45;
         System.out.println(a4.getPrintData());
-        
-                
-    
+
     }
 }
